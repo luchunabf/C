259a46b17822bbafc22634a1ff14f593ee232bc5
PNUHC <luchunabf@163.com>
2019-04-14 21:03:41 +0800

    日常练习

---
+++ test_4_14_lianxi_5/test_4_14_liaxni_5/lianxi5.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int i = -20;
+	unsigned int j = 10;
+	printf("%d\n", i + j);
+	system("pause");
+	return 0;
+}
