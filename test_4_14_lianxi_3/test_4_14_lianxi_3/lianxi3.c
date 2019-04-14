7ae92014d495bf72011c1439808b02ab9acaef6d
PNUHC <luchunabf@163.com>
2019-04-14 21:02:53 +0800

    日常练习

---
+++ test_4_14_lianxi_3/test_4_14_lianxi_3/lianxi3.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	char a = -1;
+	signed char b = -1;
+	unsigned char c = -1;
+	printf("a=%d,b=%d,c=%d\n", a, b, c);
+	system("pause");
+	return 0;
+}
