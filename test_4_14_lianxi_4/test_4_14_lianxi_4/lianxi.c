ab9719f97ec10d1175c796f7a00b2c81fe4d625c
PNUHC <luchunabf@163.com>
2019-04-14 21:03:17 +0800

    日常练习

---
+++ test_4_14_lianxi_4/test_4_14_lianxi_4/lianxi.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+//int main()
+//{
+//	char a = 128;
+//	printf("%u\n", a);
+//	system("pause");
+//	return 0;
+//}
+int main()
+{
+	char a = -128;
+	printf("%u\n", a);
+	system("pause");
+	return 0;
+}
