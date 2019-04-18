a0e31e991564eff05abd2e18aa6d13143bba2c8e
PNUHC <luchunabf@163.com>
2019-04-18 23:50:29 +0800

    日常练习

---
+++ test_4_18_lianxi_7/test_4_18_lianxi_7/main7.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	printf("请输入a和b的值：\n");
+	scanf("%d %d", &a, &b);
+	int ret = ((a + b) >> 1);
+	for (int i = 0; i < ret; i++)
+	{
+		Sleep(500);
+		printf("%d\n", i);
+		printf("I Love Phoner\n");
+	}
+	system("pause");
+	return 0;
+
+}
