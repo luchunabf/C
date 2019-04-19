18263c517b1945aa785014ca9832b79d5aa0028e
PNUHC <luchunabf@163.com>
2019-04-19 23:54:28 +0800

    优化代码

--- test_4_19_lianxi_4/test_4_19_lianxi_4/04194.c
+++ test_4_19_lianxi_4/test_4_19_lianxi_4/04194.c
@@ -1,17 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<stdlib.h>
+//int main()
+//{
+//	int a = 18;
+//	int b = 24;
+//	int tmp = 0;
+//	while (tmp = a%b)
+//	{
+//		a = b;
+//		b = tmp;
+//	}
+//	printf("%d\n", b);
+//	system("pause");
+//	return 0;
+//}
+
 int main()
 {
-	int a = 18;
-	int b = 24;
-	int tmp = 0;
-	while (tmp = a%b)
-	{
-		a = b;
-		b = tmp;
-	}
-	printf("%d\n", b);
+	int a = 10;
+	int*p = &a;
+
+	*p = 20;
+	printf("%d\n", a);
 	system("pause");
 	return 0;
 }
