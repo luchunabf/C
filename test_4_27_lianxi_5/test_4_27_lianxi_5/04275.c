4b5a2612a420781ae11a922679ca2261e1e795fb
PNUHC <luchunabf@163.com>
2019-04-27 23:52:30 +0800

    日常练习-指针练习题5-PNUHC

--- test_4_27_lianxi_5/test_4_27_lianxi_5/04275.c
+++ test_4_27_lianxi_5/test_4_27_lianxi_5/04275.c
@@ -5,8 +5,8 @@ int main()
 {
 	int a[5][5];
 	int(*p)[4];//数组指针，数组列为4，行视情况往后补
-	p = a;
+	p = a;//a是数组指针，类型int(*p)[5];强转：p=(int(*)[4])a
 	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);//18-22=-4   (-4)的地址是FF FF FF FC   (-1)的地址十六进制全为F：FF FF FF FF
-	system("pause");
+	system("pause");//p和a差4个对象，即16个字节
 	return 0;
 }
