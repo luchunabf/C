e2492396be3d2c7849f6bba2ca0915968d462bf9
PNUHC <luchunabf@163.com>
2019-04-28 22:36:03 +0800

    优化代码

--- test_4_28_lianxi_2/test_4_28_lianxi_2/04282.c
+++ test_4_28_lianxi_2/test_4_28_lianxi_2/04282.c
@@ -6,8 +6,8 @@ int main()
 	char* c[] = { "ENTER", "NEW", "POINT", "FIRST" };//一级字符指针数组，数组里面的元素是E,N,P,F（首个字符）的地址，地址指向"ENTER", "NEW", "POINT", "FIRST"
 	///////并非直接存放"ENTER", "NEW", "POINT", "FIRST"，存的是它们的地址
 	char** cp[] = { c + 3, c + 2, c + 1, c };//二级字符指针数组，解释同上
-	char*** cpp = cp;//
-	printf("%s\n", **++cpp);//point
+	char*** cpp = cp;//三级指针，指针的地址的地址
+	printf("%s\n", **++cpp);//POINT
 	printf("%s\n", *--*++cpp + 3);
 	printf("%s\n", *cpp[-2] + 3);
 	printf("%s\n", cpp[-1][-1] + 1);
