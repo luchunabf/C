95b62060cf6672cd29c5d7333b178adc903171ff
PNUHC <luchunabf@163.com>
2019-05-02 23:55:45 +0800

    日常练习——PNUHC

---
+++ test_5_2_lianxi_6/test_5_2_lianxi_6/05026.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+size_t my_strlen(const char* str)
+{
+	assert(str);
+	const char* p = str;
+	while (*str != '\0')
+	{
+		str++;
+	}
+	return str-p;
+}
+int main()
+{
+	char* a = "abcdfghj";
+	size_t ret = my_strlen(a);
+	printf("%d\n", ret);
+	system("pause");
+	return 0;
+
+
+}
