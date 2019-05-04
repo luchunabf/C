7e07d981cea7e4270ce8fe1fdb0fab787007e4a3
PNUHC <luchunabf@163.com>
2019-05-04 23:34:43 +0800

    日常练习-PNUHC

---
+++ test_5_4_lianxi_5/test_5_4_lianxi_5/05045.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int check_sys()
+{
+	union
+	{
+		int i;
+		char c;
+	}un;
+	un.i = 1;
+	return un.c;
+}
+int main()
+{
+	int ret = check_sys();
+	if (ret == 1)
+		printf("Ð¡¶Ë\n");
+	else
+		printf("´ó¶Ë\n");
+	system("pause");
+	return 0;
+}
