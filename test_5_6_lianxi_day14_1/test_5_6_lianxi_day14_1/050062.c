d33fca9ce75c1eb986cd8c979ff69350db7c7af6
PNUHC <luchunabf@163.com>
2019-05-06 23:08:25 +0800

    作业练习-PNUHC

---
+++ test_5_6_lianxi_day14_1/test_5_6_lianxi_day14_1/050062.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<Windows.h>
+#include<assert.h>
+char* left_switch(char* str, int n)
+{
+	assert(str);
+	assert(n > 0);
+	int len = strlen(str);
+	int top = n%len;
+	for (int i = 0; i < top; i++)
+	{
+		char first = str[0];
+		int j = 0;
+		for (; j < len - 1; j++)
+		{
+			str[j] = str[j + 1];
+		}
+		str[j] = first;
+	}
+	return str;
+}
+int main()
+{
+	char str[] = "ABCD";
+	int n = 3;
+	char* p = left_switch(str, n);
+	printf("%s", p);
+	system("pause");
+	return 0;
+}
