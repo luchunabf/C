1c19889be7da430d82704c73101fc833654692b9
PNUHC <luchunabf@163.com>
2019-05-06 23:08:48 +0800

    作业练习-PNUHC

---
+++ test_5_6_lianxi_day15_1/test_5_6_lianxi_day15_1/05063.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 4, 3, 2, 1 };
+	for (int i = 0; i < 10; i++)
+	{
+		int flag = 0;
+		for (int j = 0; j < 10; j++)
+		{
+			if ((arr[i] == arr[j]) && (i != j))
+				flag++;
+		}
+		if (flag == 0)
+			printf("%d\n", arr[i]);
+	}
+	system("pause");
+	return 0;
+}
