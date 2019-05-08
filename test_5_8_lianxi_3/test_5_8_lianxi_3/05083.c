f027d6583c1e9edfb2ec3558f16289276b12c40e
PNUHC <luchunabf@163.com>
2019-05-09 00:03:33 +0800

    日常练习

---
+++ test_5_8_lianxi_3/test_5_8_lianxi_3/05083.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<assert.h>
+char* my_strcat1(char* dst, const char* src)
+{
+	assert(dst && src);
+	char* ret = dst;
+	while (*dst++);
+	dst--;
+	while (*dst++ = *src++);
+	return ret;
+}
+char* my_strcat2(char* dst, const char* src)
+{
+	assert(src && dst);
+	char* ret = dst;
+	size_t count = 0;
+	while (dst[count])
+	{
+		count++;
+	}
+	dst += count;
+	while (*dst++ = *src++);
+	return ret;
+}
+int main()
+{
+	char a1[20] = "I";
+	char a2[20] = "I";
+	char* b = " Love you";
+	printf("%s\n", my_strcat1(a1, b));
+	printf("%s\n", my_strcat2(a2, b));
+	system("pause");
+	return 0;
+}
