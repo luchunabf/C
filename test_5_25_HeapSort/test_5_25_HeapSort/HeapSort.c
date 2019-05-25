80786779b1f1b96f702b4c0614b20bcd29952364
PNUHC <luchunabf@163.com>
2019-05-25 23:19:08 +0800

    堆排序-PNUHC

---
+++ test_5_25_HeapSort/test_5_25_HeapSort/HeapSort.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <malloc.h>
+#include<stdlib.h>
+void HeapAdjust(int a[], int s, int m)//一次筛选的过程
+{
+	int rc, j;
+	rc = a[s];
+	for (j = 2 * s; j <= m; j = j * 2)//通过循环沿较大的孩子结点向下筛选
+	{
+		if (j<m&&a[j]<a[j + 1]) j++;//j为较大的记录的下标
+		if (rc>a[j]) break;
+		a[s] = a[j]; s = j;
+	}
+	a[s] = rc;//插入
+}
+void HeapSort(int a[], int n)
+{
+	int temp, i;
+	for (i = n / 2; i>0; i--)//通过循环初始化顶堆
+	{
+		HeapAdjust(a, i, n);
+	}
+	for (i = n; i > 0; i--)
+	{
+		temp = a[1];
+		a[1] = a[i];
+		a[i] = temp;//将堆顶记录与未排序的最后一个记录交换
+		HeapAdjust(a, 1, i - 1);//重新调整为顶堆
+	}
+}
+int main()
+{
+	int n = 10, i = 1;
+	//scanf("%d", &n);
+	int a[11] = { 0 };
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	HeapSort(a, n);
+	for (int j = 1; j <= n; j++)
+		printf("%d ", a[j]);
+	system("pause");
+	return 0;
+}
