913141f326f6af8564beec6155cbdf0f698b9ba5
PNUHC <luchunabf@163.com>
2019-09-01 23:49:38 +0800

    item1

---
+++ test_8_7_item1/test_8_7_item1/main.cpp
@@ -0,0 +1,355 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <windows.h>
+#include <tchar.h>
+
+//int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+//{
+//	::MessageBox(NULL, _T("Hello World !"), NULL, NULL);
+//	return 0;
+//}
+#pragma once
+#include <UIlib.h>
+using namespace DuiLib;
+
+#ifdef _DEBUG
+#   ifdef _UNICODE
+#       pragma comment(lib, "DuiLib_ud.lib")
+#   else
+#       pragma comment(lib, "DuiLib_d.lib")
+#   endif
+#else
+#   ifdef _UNICODE
+#       pragma comment(lib, "DuiLib_u.lib")
+#   else
+#       pragma comment(lib, "DuiLib.lib")
+#   endif
+#endif
+
+
+#if 0
+class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual void    Notify(TNotifyUI& msg) {}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+
+		if (uMsg == WM_CREATE)
+		{
+			CControlUI *pWnd = new CButtonUI;
+			pWnd->SetText(_T("Hello World"));   // 设置文字
+			pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
+
+			m_PaintManager.Init(m_hWnd);
+			m_PaintManager.AttachDialog(pWnd);
+			return lRes;
+		}
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
+#endif
+
+
+#if 0
+class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual void    Notify(TNotifyUI& msg)
+	{
+		if (msg.sType == _T("click"))
+		{
+			if (msg.pSender->GetName() == _T("btnHello"))
+			{
+				::MessageBox(NULL, _T("我是按钮"), _T("点击了按钮"), NULL);
+			}
+		}
+	}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+
+		if (uMsg == WM_CREATE)
+		{
+			CControlUI *pWnd = new CButtonUI;
+			pWnd->SetName(_T("btnHello"));      // 设置控件的名称，这个名称用于标识每一个控件，必须唯一，相当于MFC里面的控件ID
+			pWnd->SetText(_T("Hello World"));   // 设置文字
+			pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
+
+			m_PaintManager.Init(m_hWnd);
+			m_PaintManager.AttachDialog(pWnd);
+			m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
+			return lRes;
+		}
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+	return 0;
+}
+#endif
+
+#if 0
+class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual void    Notify(TNotifyUI& msg)
+	{
+		if (msg.sType == _T("click"))
+		{
+			if (msg.pSender->GetName() == _T("btnHello"))
+			{
+				::MessageBox(NULL, _T("我是按钮"), _T("点击了按钮"), NULL);
+			}
+		}
+	}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+
+		if (uMsg == WM_CREATE)
+		{
+			CControlUI *pWnd = new CButtonUI;
+			pWnd->SetName(_T("btnHello"));      // 设置控件的名称，这个名称用于标识每一个控件，必须唯一，相当于MFC里面的控件ID
+			pWnd->SetText(_T("Hello World"));   // 设置文字
+			pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
+
+			m_PaintManager.Init(m_hWnd);
+			m_PaintManager.AttachDialog(pWnd);
+			m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
+			return lRes;
+		}
+
+		// 以下3个消息WM_NCACTIVATE、WM_NCCALCSIZE、WM_NCPAINT用于屏蔽系统标题栏
+		else if (uMsg == WM_NCACTIVATE)
+		{
+			if (!::IsIconic(m_hWnd))
+			{
+				return (wParam == 0) ? TRUE : FALSE;
+			}
+		}
+		else if (uMsg == WM_NCCALCSIZE)
+		{
+			return 0;
+		}
+		else if (uMsg == WM_NCPAINT)
+		{
+			return 0;
+		}
+
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.ShowModal();
+	return 0;
+}
+#endif
+
+
+#if 0
+class CDuiFrameWnd : public CWindowWnd, public INotifyUI
+{
+public:
+	virtual LPCTSTR GetWindowClassName() const { return _T("DUIMainFrame"); }
+	virtual void    Notify(TNotifyUI& msg)
+	{
+		if (msg.sType == _T("click"))
+		{
+			if (msg.pSender->GetName() == _T("btnHello"))
+			{
+				::MessageBox(NULL, _T("我是按钮"), _T("点击了按钮"), NULL);
+			}
+		}
+	}
+
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+	{
+		LRESULT lRes = 0;
+
+		if (uMsg == WM_CREATE)
+		{
+			//CControlUI *pWnd = new CButtonUI;
+			//pWnd->SetName(_T("btnHello"));      // 设置控件的名称，这个名称用于标识每一个控件，必须唯一，相当于MFC里面的控件ID
+			//pWnd->SetText(_T("Hello World"));   // 设置文字
+			//pWnd->SetBkColor(0xFF00FF00);       // 设置背景色
+
+			//m_PaintManager.Init(m_hWnd);
+			//m_PaintManager.AttachDialog(pWnd);
+			//m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
+			//return lRes;
+			m_PaintManager.Init(m_hWnd);
+
+			CDialogBuilder builder;
+			CControlUI* pRoot = builder.Create(_T("duilib.xml"), (UINT)0, NULL, &m_PaintManager);   // duilib.xml需要放到exe目录下
+			ASSERT(pRoot && "Failed to parse XML");
+
+			m_PaintManager.AttachDialog(pRoot);
+			m_PaintManager.AddNotifier(this);   // 添加控件等消息响应，这样消息就会传达到duilib的消息循环，我们可以在Notify函数里做消息处理
+			return lRes;
+		}
+
+		// 以下3个消息WM_NCACTIVATE、WM_NCCALCSIZE、WM_NCPAINT用于屏蔽系统标题栏
+		else if (uMsg == WM_NCACTIVATE)
+		{
+			if (!::IsIconic(m_hWnd))
+			{
+				return (wParam == 0) ? TRUE : FALSE;
+			}
+		}
+		else if (uMsg == WM_NCCALCSIZE)
+		{
+			return 0;
+		}
+		else if (uMsg == WM_NCPAINT)
+		{
+			return 0;
+		}
+
+
+		if (m_PaintManager.MessageHandler(uMsg, wParam, lParam, lRes))
+		{
+			return lRes;
+		}
+
+		return __super::HandleMessage(uMsg, wParam, lParam);
+	}
+
+protected:
+	CPaintManagerUI m_PaintManager;
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+	CPaintManagerUI::SetResourcePath(CPaintManagerUI::GetInstancePath());   // 设置资源的默认路径（此处设置为和exe在同一目录）
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();//使窗口居中
+	duiFrame.ShowModal();
+	return 0;
+}
+#endif
+
+
+
+
+
+
+
+
+#if 0
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	virtual LPCTSTR    GetWindowClassName() const   { return _T("DUIMainFrame"); }
+	//virtual CDuiString GetSkinFile()                { return _T("duilib.xml"); }
+	virtual CDuiString GetSkinFile()                { return _T("test_1.xml"); }
+	virtual CDuiString GetSkinFolder()              { return _T(""); }
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();
+	duiFrame.ShowModal();
+	return 0;
+}
+#endif 
+
+
+
+class CDuiFrameWnd : public WindowImplBase
+{
+public:
+	virtual LPCTSTR    GetWindowClassName() const   { return _T("DUIMainFrame"); }
+	//virtual CDuiString GetSkinFile()                { return _T("duilib.xml"); }
+	virtual CDuiString GetSkinFile()                { return _T("test_1.xml"); }
+	virtual CDuiString GetSkinFolder()              { return _T(""); }
+
+	virtual void  InitWindow()
+	{
+		CActiveXUI* pActiveXUI = static_cast<CActiveXUI*>(m_PaintManager.FindControl(_T("ActiveXDemo1")));
+
+		if (pActiveXUI)
+		{
+			IWebBrowser2* pWebBrowser = NULL;
+
+			pActiveXUI->SetDelayCreate(false);              // 相当于界面设计器里的DelayCreate属性改为FALSE，在duilib自带的FlashDemo里可以看到此属性为TRUE             
+			pActiveXUI->CreateControl(CLSID_WebBrowser);    // 相当于界面设计器里的Clsid属性里填入{8856F961-340A-11D0-A96B-00C04FD705A2}，建议用CLSID_WebBrowser，如果想看相应的值，请见<ExDisp.h>
+			pActiveXUI->GetControl(IID_IWebBrowser2, (void**)&pWebBrowser);
+
+			if (pWebBrowser != NULL)
+			{
+				//pWebBrowser->Navigate(L"https://code.google.com/p/duilib/",NULL,NULL,NULL,NULL);  
+				pWebBrowser->Navigate(L"http://www.baidu.com/", NULL, NULL, NULL, NULL);  // 由于谷歌时不时被墙，所以换成反应快的网站
+				pWebBrowser->Release();
+			}
+		}
+	}
+};
+
+int APIENTRY _tWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow)
+{
+	CPaintManagerUI::SetInstance(hInstance);
+	HRESULT Hr = ::CoInitialize(NULL);
+	if (FAILED(Hr)) return 0;
+
+
+	CDuiFrameWnd duiFrame;
+	duiFrame.Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame.CenterWindow();
+	duiFrame.ShowModal();
+
+	::CoUninitialize();
+	return 0;
+}
